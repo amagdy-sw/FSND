5fe1334a3f7a9a6e295790e8a6cb8f8b4d3f5b7a
amagdy-sw <68121587+amagdy-sw@users.noreply.github.com>
2020-09-30 20:14:58 +0200

    finalize project
    finalize project

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -81,7 +81,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(data['deleted'],question_id)
-        self.assertEqual(question, None)
+        self.assertIsNone(question)
 
     def test_422_delete_question(self):
         res = self.client().delete('/questions/0')
